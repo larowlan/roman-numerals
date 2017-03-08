994ce013c6326b252a8f6c999d744f99a96d62f2
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-03-09 07:40:32 +1000

    Initial tests

---
+++ src/RomanNumeralGenerator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Larowlan\RomanNumeral;
+
+/**
+ * Defines a class for generating roman numerals from integers.
+ */
+class RomanNumeralGenerator {
+
+  /**
+   * Generates a roman numeral from an integer.
+   *
+   * @param int $number
+   *   Integer to convert.
+   * @param bool $lowerCase
+   *   (optional) Pass TRUE to convert to lowercase. Defaults to FALSE.
+   *
+   * @return string
+   *   Roman numeral representing the passed integer.
+   */
+  public function generate(int $number, bool $lowerCase = FALSE) : string {
+    return "Not implemented";
+  }
+
+}
