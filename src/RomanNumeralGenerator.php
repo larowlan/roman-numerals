e87cb94b4a6127feb43a8410ccf67adc34d2f83b
althaf hassan pulikunnu abdulkader <altha1981@gmail.com>
2018-10-15 11:01:49 +1100

    althafH generate roman numerals with test

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -6,7 +6,7 @@ namespace Larowlan\RomanNumeral;
  * Defines a class for generating roman numerals from integers.
  */
 class RomanNumeralGenerator {
-
+	
   /**
    * Generates a roman numeral from an integer.
    *
@@ -19,7 +19,27 @@ class RomanNumeralGenerator {
    *   Roman numeral representing the passed integer.
    */
   public function generate(int $number, bool $lowerCase = FALSE) : string {
-    return "Not implemented";
+	  
+     $n = intval($number); 
+     $result = ''; 
+  
+     $lookup = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 
+     'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 
+     'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1); 
+	 
+	 if($lowerCase){
+		$lookup = array_change_key_case($lookup,CASE_LOWER);
+	 }
+	 
+     foreach ($lookup as $roman => $value)  
+     { 
+         $matches = intval($n / $value); 
+         $result .= str_repeat($roman, $matches); 
+         $n = $n % $value; 
+     } 
+
+	 return $result;
+    
   }
 
 }
--- tests/RomanNumeralGeneratorTest.php
+++ tests/RomanNumeralGeneratorTest.php
@@ -34,6 +34,15 @@ class RomanNumeralGeneratorTest extends \PHPUnit_Framework_TestCase {
   public function testGeneration($number, $expected) {
     $this->assertEquals($expected, $this->generator->generate($number));
   }
+  
+  /**
+   * Tests lower case roman numeral generation.
+   *
+   * @dataProvider providerTestGenerationLower
+   */
+  public function testGenerationLower($number, $expected) {
+    $this->assertEquals($expected, $this->generator->generate($number,TRUE));
+  }
 
   /**
    * Data provider for testGeneration().
@@ -61,6 +70,35 @@ class RomanNumeralGeneratorTest extends \PHPUnit_Framework_TestCase {
       911 => [911, "CMXI"],
       1024 => [1024, "MXXIV"],
       3000 => [3000, "MMM"],
+    ];
+  }
+  
+  /**
+   * Data provider for testGenerationLower().
+   *
+   * @return array
+   *   Test cases.
+   */
+  public function providerTestGenerationLower() {
+    return [
+      1 => [1, "i"],
+      2 => [2, "ii"],
+      3 => [3, "iii"],
+      4 => [4, "iv"],
+	  5 => [5, "v"],
+      6 => [6, "vi"],
+      9 => [9, "ix"],
+      27 => [27, "xxvii"],
+      48 => [48, "xlviii"],
+      59 => [59, "lix"],
+      93 => [93, "xciii"],
+      141 => [141, "cxli"],
+      163 => [163, "clxiii"],
+      402 => [402, "cdii"],
+      575 => [575, "dlxxv"],
+      911 => [911, "cmxi"],
+      1024 => [1024, "mxxiv"],
+      3000 => [3000, "mmm"],
     ];
   }
 }
