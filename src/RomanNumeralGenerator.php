0d8e23801146904de36704d182d0dbd2c35d3c6f
Christine Roque <xtineroque@gmail.com>
2018-10-17 08:04:34 +1100

    Updated class to generate uppercase roman numerals

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -8,6 +8,26 @@ namespace Larowlan\RomanNumeral;
 class RomanNumeralGenerator {
 
   /**
+   * @var $roman_numerals
+   *   A map of roman numerals and the number equivalent.
+   */
+  private $roman_numerals = [
+    'M' => 1000, 
+    'CM' => 900, 
+    'D' => 500, 
+    'CD' => 400, 
+    'C' => 100, 
+    'XC' => 90, 
+    'L' => 50, 
+    'XL' => 40, 
+    'X' => 10, 
+    'IX' => 9, 
+    'V' => 5, 
+    'IV' => 4, 
+    'I' => 1
+  ];
+
+  /**
    * Generates a roman numeral from an integer.
    *
    * @param int $number
@@ -19,7 +39,17 @@ class RomanNumeralGenerator {
    *   Roman numeral representing the passed integer.
    */
   public function generate(int $number, bool $lowerCase = FALSE) : string {
-    return "Not implemented";
+
+     $roman_numeral = '';
+
+     foreach ($this->roman_numerals as $roman => $numeral) {
+        $match = ($number / $numeral);
+        $roman_numeral .= str_repeat($roman, $match);
+        $number = ($number % $numeral);
+     }
+
+     return ($lowerCase) ? strtolower($roman_numeral) : $roman_numeral;
+
   }
 
 }
